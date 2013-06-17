ae175f30cb6cd444d805baf7ad06f1f4c9457ab2
Rafael <rsk@bccn-berlin.de>
2013-06-17 14:29:35 +0100

    Linebreak in docstring added.

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import find_packages, setup
 
 
 DESCRIPTION = 	"""\
-Basic wrapper to return datasets from the Quandl website as Pandas dataframe objects with a timeseries index, or as a numpy array.
+Basic wrapper to return datasets from the Quandl website as Pandas
+dataframe objects with a timeseries index, or as a numpy array.
 """
 
 setup(
