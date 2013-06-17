36f756a3ef214cd6c0e578c515da975da3c416c9
Michele Orrù <maker@python.it>
2013-06-17 14:16:58 +0100

    Importing setup.py from pypi.
    Downlaoding setup.py from the released version 1.5 available on pypi.
    Adding metadata about the authors, license, emails, etc. at package level.
    Using setuptools, if available.
    XXX. A warning is going to be issued if distutils is used, since
    `install_requires` is setuptools-only.

--- Quandl/__init__.py
+++ Quandl/__init__.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
-__author__ = 'mhartney & ChrisStevens'
-__all__ = ['Quandl']
 
+__authors__ = (
+    'Mark Hartney',
+    'Chris Stevens',
+)
+__maintainer__ = 'Chris Stevens'
+__email__ = 'connect@quandl.com'
+__url__ = 'http://www.quandl.com/'
+__license__ = 'MIT'
+__version__ = '1.5'
 
 from .Quandl import get, push, search
