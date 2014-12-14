efa9858dca1b57b3a5c439cca2b5c2fd2e0d81fa
Hobson Lane <hobsonlane@gmail.com>
2014-12-13 20:54:17 -0800

    Fix setup.py
    * make requirements.txt match more recent versions in setup.py
    * read requirements.txt to get versions for dependency list in setup.py
    * don't import dependences in Quandl.__init__.py until you've had a chance to install them!

--- Quandl/__init__.py
+++ Quandl/__init__.py
@@ -10,17 +10,25 @@ __url__ = 'http://www.quandl.com/'
 __license__ = 'MIT'
 __version__ = '1.5'
 
-from .Quandl import (
-    get,
-    push,
-    search,
-    WrongFormat,
-    MultisetLimit,
-    ParsingError,
-    CallLimitExceeded,
-    DatasetNotFound,
-    ErrorDownloading,
-    MissingToken,
-    DateNotRecognized,
-    CodeFormatError
-    )
+#############################################
+#
+# Don't import anything in __init__.py!
+# Otherwise setup.py can't import the private constants above (__authors__, etc)
+# because it won't have installed dependencies yet!
+#
+# from .Quandl import (
+#     get,
+#     push,
+#     search,
+#     WrongFormat,
+#     MultisetLimit,
+#     ParsingError,
+#     CallLimitExceeded,
+#     DatasetNotFound,
+#     ErrorDownloading,
+#     MissingToken,
+#     DateNotRecognized,
+#     CodeFormatError
+#     )
+#
+###############################################
--- setup.py
+++ setup.py
@@ -1,21 +1,38 @@
+from Quandl import __version__, __authors__, __github_url__, __maintainer__, __email__, __url__, __license__
+from Quandl import __name__ as package_name
+import os
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-import Quandl
+try:
+    from pip.req import parse_requirements
+    requirements = list(parse_requirements('requirements.txt'))
+except:
+    requirements = []
+install_requires=[str(req).split(' ')[0].strip() for req in requirements if req.req and not req.url]
+print 'Install requirements found in requirements.txt: %r' % install_requires
+dependency_links=[req.url for req in requirements if req.url]
+print 'Dependencies found in requirements.txt: %r' % dependency_links
 
-setup(name = 'Quandl',
+
+setup(
+      name = package_name,
       description = 'Package for Quandl API access',
-      version = Quandl.__version__,
-      author = ", ".join(Quandl.__authors__),
-      maintainer = Quandl.__maintainer__,
-      maintainer_email = Quandl.__email__,
-      url = Quandl.__url__,
-      license = Quandl.__license__,
+      long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+      version = __version__,
+      author = ", ".join(__authors__),
+      maintainer = __maintainer__,
+      maintainer_email = __email__,
+      url = __url__,
+      license = __license__,
       install_requires = [
-      "pandas >= 0.14",
-      "numpy >= 1.8",
+        "pandas >= 0.14",
+        "numpy >= 1.8",
       ],
+      # install_requires = install_requires,
+      dependency_links = dependency_links,
       packages = ['Quandl'],
 )
