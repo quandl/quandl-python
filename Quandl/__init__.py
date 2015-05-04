d0e30ec29822a21bad628f0ec7e8de1f3709c8fa
ChrisStevens <Chris98@gmail.com>
2015-05-04 16:22:25 -0400

    Proper version, in line with pypi.

--- Quandl/__init__.py
+++ Quandl/__init__.py
@@ -8,7 +8,7 @@ __maintainer__ = 'Chris Stevens'
 __email__ = 'connect@quandl.com'
 __url__ = 'http://www.quandl.com/'
 __license__ = 'MIT'
-__version__ = '1.5'
+__version__ = '2.8.6'
 
 #############################################
 #
