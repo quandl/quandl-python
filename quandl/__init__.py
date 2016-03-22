af04695e9130ca6249757fffd957c098cdde216e
Jun Li <go2jun@gmail.com>
2016-03-22 15:37:14 -0400

    init commit

--- quandl/__init__.py
+++ quandl/__init__.py
@@ -9,3 +9,23 @@ from .model.dataset import Dataset
 from .model.datatable import Datatable
 from .model.data import Data
 from .model.merged_dataset import MergedDataset
+import copy
+
+def get_table(code, **options):
+    paginate = options.pop('paginate')
+    data = None
+    while True:
+        next_options = copy.deepcopy(options)
+        next_data = Datatable(code).data(params=next_options)
+        next_cursor_id = next_data.meta['next_cursor_id']
+        if next_cursor_id is None:
+            break
+        elif paginate is not True and next_cursor_id is not None:
+            print "for more use paginate=True"
+            break
+        options['qopts.cursor_id'] = next_cursor_id
+        if data is None:
+            data = next_data
+        else:
+            data.extend(next_data)
+    return data.to_pandas()
