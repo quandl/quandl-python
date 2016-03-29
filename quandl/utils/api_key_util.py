60f8a5eda9bf7d4c35e0d42ea176364bc036c737
Clement Leung <clement@quandl.com>
2016-03-29 11:29:31 -0400

    remove user warning

--- quandl/utils/api_key_util.py
+++ quandl/utils/api_key_util.py
@@ -1,6 +1,4 @@
 from quandl.api_config import ApiConfig
-import warnings
-import os
 
 
 class ApiKeyUtil(object):
@@ -9,9 +7,3 @@ class ApiKeyUtil(object):
         # set api key
         if 'api_key' in params:
             ApiConfig.api_key = params.pop('api_key')
-        # issue warning if api key was previously
-        # set and now is not
-        elif (ApiConfig.api_key is not None) and os.isatty(0):
-            warn_msg = "To ensure your api key is properly configured, please see: \
-https://github.com/quandl/quandl-python/blob/master/README.md"
-            warnings.warn(warn_msg, UserWarning)
