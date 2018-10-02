46df6b4b48d6ab96e5f2a3e51707450730862203
Ulf Hamster <554c46@gmail.com>
2018-10-02 16:58:19 +0200

    Issue 116 - Save and read API Key from local file (#117)
    * Issue 116 - Save and read API Key from local file
    
    * comments removed and syntax corrected
    
    * more syntax corrected

--- quandl/api_config.py
+++ quandl/api_config.py
@@ -3,3 +3,32 @@ class ApiConfig:
     api_base = 'https://www.quandl.com/api/v3'
     api_version = None
     page_limit = 100
+
+
+def save_key(apikey, filename=None):
+    if filename is None:
+        import pathlib
+        filename = str(pathlib.Path.home()) + "/.quandl_apikey"
+
+    fileptr = open(filename, 'w')
+    fileptr.write(apikey)
+    fileptr.close()
+    ApiConfig.api_key = apikey
+
+
+def read_key(filename=None):
+    if filename is None:
+        import pathlib
+        filename = str(pathlib.Path.home()) + "/.quandl_apikey"
+
+    try:
+        fileptr = open(filename, 'r')
+        apikey = fileptr.read()
+        fileptr.close()
+
+        if apikey:
+            ApiConfig.api_key = apikey
+        else:
+            raise Exception("File '{:s}' is empty.".format(filename))
+    except ValueError:
+        raise Exception("File '{:s}' not found.".format(filename))
