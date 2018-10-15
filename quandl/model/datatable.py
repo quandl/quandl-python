1c94ae379bb74b6b58dd6536fab3b98736f466e1
Shuo Feng <shuo@quandl.com>
2018-10-15 16:14:01 -0400

    small funtion update

--- quandl/model/datatable.py
+++ quandl/model/datatable.py
@@ -38,39 +38,43 @@ class Datatable(GetOperation, ListOperation, ModelBase):
         if 'params' not in options:
             options['params'] = {}
 
-        return self._url_request(file_or_folder_path, **options['params'])
+        return self._request_file_info(file_or_folder_path, **options['params'])
 
-    def _url_request(self, file_or_folder_path, **options):
+    def _request_file_info(self, file_or_folder_path, **options):
         url = self._download_request_path(**options)
         code_name = self.code
 
         r = Connection.request('get', url, **options)
         response_data = r.json()
 
-        status = response_data['datatable_bulk_download']['file']['status']
+        file_info = response_data['datatable_bulk_download']['file']
+
+        status = file_info['status']
 
         if status == 'fresh':
-            file_link = response_data['datatable_bulk_download']['file']['link']
-
-            file_path = file_or_folder_path
-            if os.path.isdir(file_or_folder_path):
-                file_path = os.path.join(file_or_folder_path,
-                                         '{}.{}'.format(code_name.replace('/', '_'), 'zip'))
-
-            res = urlopen(file_link)
-
-            with open(file_path, 'wb') as fd:
-                while True:
-                    chunk = res.read(self.BULK_CHUNK_SIZE)
-                    if not chunk:
-                        break
-                    fd.write(chunk)
-
-            return file_path
+            file_link = file_info['link']
+            return self._download_file_with_link(file_or_folder_path, file_link, code_name)
         else:
             print(Message.LONG_GENERATION_TIME)
             sleep(self.WAIT_GENERATION_INTERVAL)
             self._url_request(file_or_folder_path, **options)
+
+    def _download_file_with_link(self, file_or_folder_path, file_link, code_name):
+        file_path = file_or_folder_path
+        if os.path.isdir(file_or_folder_path):
+            file_path = os.path.join(file_or_folder_path,
+                                     '{}.{}'.format(code_name.replace('/', '_'), 'zip'))
+
+        res = urlopen(file_link)
+
+        with open(file_path, 'wb') as fd:
+            while True:
+                chunk = res.read(self.BULK_CHUNK_SIZE)
+                if not chunk:
+                    break
+                fd.write(chunk)
+
+        return file_path
 
     def _download_request_path(self, **options):
         url = self.default_path()
