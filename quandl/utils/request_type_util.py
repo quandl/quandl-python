9b6487c17936a73be7931aa7e26e9f3cc37a1d83
Justin Martin <justinmartin@quandl.com>
2018-11-19 13:31:46 -0500

    Add sanity tests for function determining request type

--- quandl/utils/request_type_util.py
+++ quandl/utils/request_type_util.py
@@ -10,7 +10,7 @@ class RequestType(object):
     MAX_URL_LENGTH_FOR_GET = 8000
 
     @classmethod
-    def get_request_type(cls,url, **params):
+    def get_request_type(cls, url, **params):
         query_string = urlencode(params['params'])
         request_url = '%s/%s/%s' % (ApiConfig.api_base, url, query_string)
         if len(request_url) < cls.MAX_URL_LENGTH_FOR_GET:
---
+++ test/test_request_type_util.py
@@ -0,0 +1,30 @@
+import unittest
+import random
+from quandl.utils.request_type_util import RequestType
+from test.helpers.string_helper import generate_random_string
+
+
+class RequestTypeUtilTest(unittest.TestCase):
+
+    def setUp(self):
+        self.test_url = '/datables/WIKI/PRICES.json'
+        RequestType.MAX_URL_LENGTH_FOR_GET = 200
+
+    def tearDown(self):
+        RequestType.MAX_URL_LENGTH_FOR_GET = 8000
+
+    def test_no_params(self):
+        request_type = RequestType.get_request_type(self.test_url, params={})
+        self.assertEqual(request_type, 'get')
+
+    def test_small_params(self):
+        params = {'foo': 'bar', 'qopts': {'columns': 'date'}}
+        request_type = RequestType.get_request_type(self.test_url, params=params)
+        self.assertEqual(request_type, 'get')
+
+    def test_long_params(self):
+        params = dict()
+        for _ in range(20):
+            params[generate_random_string()] = generate_random_string()
+        request_type = RequestType.get_request_type(self.test_url, params=params)
+        self.assertEqual(request_type, 'post')
